3743c30e4c8c56620e9e9501a16631e7ff9b3377
guohong07@DESKTOP-HR7AKB3_WSL2 <xieg678@gmail.com>
2023-06-12 19:48:03 +0800

    feat(base): create folder src/base/ - created flles: thread noncopyable timestamp, etc.

---
+++ src/base/noncopyable.h
@@ -0,0 +1,19 @@
+#ifndef RYANLIB_BASE_NONCOPYABLE_H_
+#define RYANLIB_BASE_NONCOPYABLE_H_
+
+//禁止拷贝的基类，设置为protected权限的成员函数可以让派生类继承
+//派生类对象可以正常的构造和析构
+
+// TODO: noncopyable 的学习
+
+class Noncopyable {
+ public:
+  Noncopyable(const Noncopyable&) = delete;
+  Noncopyable& operator=(const Noncopyable&) = delete;
+
+ private:
+  Noncopyable() = default;
+  ~Noncopyable() = default;
+};
+
+#endif //RYANLIB_BASE_NONCOPYABLE_H_
---
+++ src/base/timestamp.h
@@ -0,0 +1,63 @@
+#ifndef RYANLIB_BASE_TIMESTAMP_H_
+#define RYANLIB_BASE_TIMESTAMP_H_
+
+#include <string>
+#include <iostream>
+#include <sys/time.h>
+class Timestamp {
+ public:
+  Timestamp() : micro_seconds_since_epoch_(0) {}
+  explicit Timestamp(int64_t micro_seconds_since_epoch) 
+      : micro_seconds_since_epoch_(micro_seconds_since_epoch) {}
+
+  // 获取当前时间戳
+  static Timestamp Now();
+
+  //用std::string形式返回，格式[millisec].[microsec]
+  std::string ToString() const;
+  //格式, "%4d年%02d月%2d日 星期%d %02d:%02d:%02d.%06d", 时分秒.微秒
+  std::string ToFormattedString(bool show_micro_seconds = false) const;
+
+  //返回当前时间戳的微秒
+  int64_t micro_seconds_since_epoch() const { return micro_seconds_since_epoch_;} 
+
+  //返回当前时间的秒数
+  time_t seconds_since_epoch() const {
+    return static_cast<time_t>(micro_seconds_since_epoch_ / kMicroSecondsPerSecond);
+  }
+
+  //失效的时间戳，返回一个值为0的Timestamp
+  static Timestamp Invalid() {
+    return Timestamp();
+  }
+
+
+  static const int kMicroSecondsPerSecond = 1000 * 1000;
+
+ private:
+  //自epoch开始的微妙数 
+  //UNIX时间戳：在Unix和类Unix系统中，
+  //epoch通常指的是从协调世界时（Coordinated Universal Time，UTC）
+  //的 1970 年 1 月 1 日 00:00:00 开始经过的秒数
+  int64_t micro_seconds_since_epoch_;
+};
+ 
+//这里传值或者传引用都可以，因为Timestamp类只有一个non-static成员变量
+inline bool operator<(Timestamp& lhs, Timestamp& rhs) {
+  return lhs.micro_seconds_since_epoch() < rhs.micro_seconds_since_epoch();
+}
+
+inline bool operator==(Timestamp& lhs, Timestamp& rhs) {
+  return lhs.micro_seconds_since_epoch() == rhs.micro_seconds_since_epoch();
+}
+
+//如果是重复定时任务就会对此时间戳进行增加
+inline Timestamp AddTime(Timestamp timestamp, double seconds) {
+  //将延时的秒数设置为微秒
+  int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+  //返回新增时后的时间戳
+  return Timestamp(timestamp.micro_seconds_since_epoch() + delta);
+}
+
+
+#endif //RYANLIB_BASE_TIMESTAMP_H_
