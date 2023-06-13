0742fd9f111712bd08397febb727bc2b0990c105
guohong <xieg678@gmail.com>
2023-06-13 18:11:28 +0800

    feat(logger): add folder src/logger/

---
+++ src/logger/fixed_buffer.h
@@ -0,0 +1,42 @@
+#ifndef RYANLIB_LOGGER_FIXED_BUFFER_H_
+#define RYANLIB_LOGGER_FIXED_BUFFER_H_
+
+#include <string>
+#include <cstring>
+#include <cassert>
+#include "noncopyable.h"
+
+const int kSmallBuffer = 4000;
+const int kLargeBuffer = 4000 * 1000;
+
+template <int SIZE>
+class FixedBuffer : public Noncopyable {
+ public:
+  FixedBuffer() : curr_(data_) {} //这个构造函数是什么鬼
+
+  void Append(const char* buf, size_t len) {
+    if (static_cast<size_t>(Avail()) > len) {
+      memcpy(curr_, buf, len);
+      cur_ += len;
+    }
+    //TODO:超出缓冲区长度如何处理，待补充
+
+  }
+  const char* data() const { return data_; }
+  int Length() const { return static_cast<int>(end() - data_); }
+
+  char* Aurrent() const { return curr_; }
+  int Avail() const { return static_cast<int>(end() - curr_); }
+  void Add(size_t len) { cur += len; }
+  void Reset() { curr_ = data_; }
+  void Bzero() { ::memset(&data_, sizeof(data_), 0); }
+  std::string ToString() const { return std::string(data_, Length()); }
+
+ private:
+  const char* end() const { return data_ + sizeof(data_); }
+
+  char data_[SIZE];
+  char* curr_;
+};
+
+#endif //RYANLIB_LOGGER_FIXED_BUFFER_H_
