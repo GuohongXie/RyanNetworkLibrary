e3a93ab6fc8a7114ec7c7420169b007b40a58fe2
guohong07@DESKTOP-HR7AKB3_WSL2 <xieg678@gmail.com>
2023-06-16 17:11:15 +0800

    test(base): finished the test of base/*

---
+++ src/base/test/thread_pool_test.cc
@@ -0,0 +1,31 @@
+#include "thread_pool.h"
+
+#include <cstdio>
+#include <unistd.h>
+
+#include <functional>
+
+#include "current_thread.h"
+#include "logging.h"
+
+int count = 0;
+
+void ShowInfo() { LOG_INFO << current_thread::Tid(); }
+
+void test1() {
+  ThreadPool pool;
+  pool.SetThreadSize(4);
+  for (int i = 0; i < 5000; i++) {
+    pool.Add(ShowInfo);
+  }
+  pool.Add([] { ::sleep(5); });
+  pool.Start();
+}
+
+void InitFunc() { printf("create thread %d\n", ++count); }
+
+int main() {
+  test1();
+
+  return 0;
+}
--- src/base/thread_pool.h
+++ src/base/thread_pool.h
@@ -1,15 +1,46 @@
 #ifndef RYANLIB_BASE_THREAD_POOL_H_
 #define RYANLIB_BASE_THREAD_POOL_H_
 
-#include <contition_variable>
+#include <condition_variable>
 #include <deque>
 #include <mutex>
 #include <vector>
 
-#include ""
-#include "noncopyable.h"
+#include "logging.h"
 #include "thread.h"
+#include "noncopyable.h"
 
-class ThreadPool : public No
+class ThreadPool : Noncopyable {
+ public:
+  using ThreadFunction = std::function<void()>;
+
+  explicit ThreadPool(const std::string& name = std::string("ThreadPool"));
+  ~ThreadPool();
+
+  void SetThreadInitCallback(const ThreadFunction& cb) {
+    thread_init_callback_ = cb;
+  }
+  void SetThreadSize(const int& num) { thread_size_ = num; }
+  void Start();
+  void Stop();
+
+  const std::string& name() const { return name_; }
+  size_t QueueSize() const;
+
+  void Add(ThreadFunction ThreadFunction);
+
+ private:
+  bool IsFull() const;
+  void RunInThread();
+
+  mutable std::mutex mutex_;
+  std::condition_variable cond_;
+  std::string name_;
+  ThreadFunction thread_init_callback_;
+  std::vector<std::unique_ptr<Thread>> threads_;
+  std::deque<ThreadFunction> queue_;
+  bool running_;
+  size_t thread_size_;
+};
 
 #endif  // RYANLIB_BASE_THREAD_POOL_H_
